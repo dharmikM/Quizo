897d7a4c7f05029d3b51225b9a50fd0b9a737c67
dharmikM <75500853+dharmikM@users.noreply.github.com>
2022-10-25 14:28:09 -0400

    Initial Commit

---
+++ Quizo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizo
+//
+//  Created by D M on 2022-10-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
